4d6c32c6ea0443203c009eef394c8597cf64c062
milusilu <milusilu@Kishan>
2022-03-24 23:45:12 +0530

    push

--- airbus/src/main/java/com/ltts/airbus/App.java
+++ airbus/src/main/java/com/ltts/airbus/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello mr airbus " );
+        System.out.println( "Hel.lo mr airbus " );
     }
 }
